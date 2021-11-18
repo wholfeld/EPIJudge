ea741fc6ae149e6594eb52b5c46f608a34f35651
Warrick Holfeld <warrick87@gmail.com>
2021-11-18 11:33:32 -0800

    testing judge

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def count_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
+
+    # # TODO - you fill in here.
+    # return 0
 
 
 if __name__ == '__main__':
